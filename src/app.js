da3451a743171e4c408dca915874d5460a96d1ae
James Schrader <jschrader@madisonchildrensmuseum.org>
2017-05-07 21:54:20 -0500

    Import Firebase and configure app to access remote database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import * as firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDtuyXpEPgq82CcKnnI9ah497Zpt961UWM',
+            authDomain: 'authicator.firebaseapp.com',
+            databaseURL: 'https://authicator.firebaseio.com',
+            projectId: 'authicator',
+            storageBucket: 'authicator.appspot.com',
+            messagingSenderId: '13508733370'
+      });
+    }
+
     render() {
         return (
             <View>

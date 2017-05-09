45f1604249045e16781c17592d4b8df85c9fb69b
James Schrader <jschrader@madisonchildrensmuseum.org>
2017-05-08 21:37:12 -0500

    Created LoginForm file with class component boilerplate code

---
+++ src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
+
+export default LoginForm;

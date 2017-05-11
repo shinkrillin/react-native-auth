ce777eeb47e479e9c9b292b6d309d3cd98d294b2
James Schrader <jschrader@madisonchildrensmuseum.org>
2017-05-10 19:54:32 -0500

    Added password input section

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '' };
+    state = { email: '', password: '' };
 
     render() {
         return (
@@ -16,7 +16,14 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
-                <CardSection/>
+                <CardSection>
+                    <Input
+                        placeholder="password"
+                        label="password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
 
                 <CardSection>
                     <Button>
